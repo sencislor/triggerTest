0debc7ba6c715914f6df4e74be60f7970bbcb0cb
sssfeather <sssfeather@gmail.com>
2017-03-20 22:07:47 +0800

    test

--- triggerTest/trigger_test/plot_wave.py
+++ triggerTest/trigger_test/plot_wave.py
@@ -3,9 +3,9 @@ from obspy.xseed import Parser
 from obspy.signal.spectral_estimation import PPSD
 
 #st = read("2014080316.YN.mseed").select(network='YN',channel='BHZ',station='QIJ')
-st = read("2014080316.YN.mseed")
+st = read("../seisdata/2014080316.YN.mseed")
 tr = st.select(id='YN.QIJ.00.BHZ')[0]
-tr = tr.filter('bandpass', freqmin=1, freqmax=5)
+tr = tr.filter('bandpass', freqmin=10, freqmax=20)
 tr.plot()
 
 '''
--- triggerTest/trigger_test/trigger_test4.py
+++ triggerTest/trigger_test/trigger_test4.py
@@ -1,12 +1,39 @@
 from obspy import read
 from obspy import Stream, read
 from obspy.core import UTCDateTime
+from obspy.signal.cross_correlation import templates_max_similarity
+import matplotlib.pyplot as plt
 
 st = read("../seisdata/2014080316.YN.mseed")
-tr = st.select(id='YN.QIJ.00.BHZ')
+st = st.select(id='YN.QIJ.00.BHZ')
 st.filter('bandpass', freqmin=10, freqmax=20)
 
 event_templates = []
-t = UTCDateTime("2014-08-03T08:30:19.029")
-st_ = st.select(station="QIJ").slice(t, t + 2.5)
-event_templates.apent(st_)
+t = UTCDateTime("2014-08-03T08:30:19.129")
+st_ = st.select(station="QIJ").slice(t, t + 30)
+event_templates.append(st_)
+
+start_time = st[0].stats['starttime']
+end_time   = st[0].stats['endtime']
+move_time = start_time
+
+ax = plt.subplot(111)
+plt.plot(st[0].data, 'k')
+while(move_time < end_time):
+	cv = templates_max_similarity(st, move_time, event_templates)
+	if cv >= 0.3:
+		print(cv)
+		print(move_time)
+	
+		tp = (move_time - start_time) * 100
+		ymin, ymax = ax.get_ylim()
+		plt.vlines(tp, ymin, ymax, color='r', linewidth=2)
+		
+		step = 30
+	else:
+		step = 0.1
+		
+	move_time = move_time + step
+	
+plt.show()
+
