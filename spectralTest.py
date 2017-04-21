74c607d38846706bee119372b9c56a4e63f84e0c
muly <sssfeather@yahoo.com>
2017-04-21 09:38:12 +0800

    add save ppsd

--- spectralTest.py
+++ spectralTest.py
@@ -14,14 +14,18 @@ ynpar = Parser('YN.dataless')
 #st = read('20140901.YN.XIM.mseed')
 #tr = st.select(id='YN.XIM.00.SHZ')[0]
 
-st = read('20140803.YN.CAY.mseed')
-tr = st.select(id='YN.CAY.00.BHZ')[0]
+#st = read('20140803.YN.CAY.mseed')
+#tr = st.select(id='YN.CAY.00.BHZ')[0]
+st = read('20140807.YN.FUN.mseed')
+tr = st.select(id='YN.FUN.00.BHZ')[1]
 
-#paz =ynpar.get_paz("%(network)s.%(station)s.%(location)s.%(channel)s" % tr.stats)
+paz =ynpar.get_paz("%(network)s.%(station)s.%(location)s.%(channel)s" % tr.stats)
+'''
 paz = {'sensitivity': 1258650000.0, 
        'poles': [-0.074+0.074j, -0.074-0.074j, -222+222j, -222-222j], 
        'gain': 98570.0, 
        'zeros': [0j, 0j]}
+'''
 #paz = {'sensitivity': 1258650000.0}
 
 #datalen = tr.stats.endtime - tr.stats.starttime
@@ -42,17 +46,17 @@ data remember to use the inverse as `metadata['sensitivity']`.
 ppsd = PPSD(tr.stats, paz, special_handling="convert", ppsd_length=3600.0/2)
 #ppsd = PPSD(tr.stats, paz, ppsd_length=3600.0/2)
 ppsd.add(st)
-
+ppsd.save_npz("%(network)s.%(station)s.%(location)s.%(channel)s.psd" % tr.stats)
 #print(ppsd.get_mode())
 #print(ppsd.get_mean())
 #ppsd.plot_coverage()
-
-ppsd.plot(show_mean=True, 
+ppsd.load_npz("%(network)s.%(station)s.%(location)s.%(channel)s.psd.npz" % tr.stats)
+ppsd.plot(show_mean=False, 
           show_coverage=True, 
           show_histogram=True, 
           show_percentiles=False, 
           show_mode=True,
-          mode_linewidth=4,
+          mode_linewidth=2,
           mode_color='red',
           percentiles=[0, 25, 50, 75, 100], 
           cumulative_number_of_colors=60, 
